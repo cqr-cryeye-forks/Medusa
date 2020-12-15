fde3ace3ee2263aaa8b410a0dee3be27fe3037e7
dielion <1045957478@qq.com>
2020-12-15 20:37:24 +0800

    vue

--- Vue/faceConfig.js
+++ Vue/faceConfig.js
@@ -3,7 +3,7 @@ const { baseURL } = require("./src/utils/request")
 const faceConfig = () =>{
     return{
         // 基础路径,打包发布的时候修改为后端发布服务地址
-        basePath: 
+        basePath:
         imgPath:
     }
 }
